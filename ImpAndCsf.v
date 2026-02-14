3169d8b135476438ac1c272ca0937d6db8d28157
memento-mogi <tomotomo0412mogi@gmail.com>
2026-02-15 00:36:35 +0900

    refactor: modified proof of csf_eq_com

--- ImpAndCsf.v
+++ ImpAndCsf.v
@@ -1,4 +1,6 @@
+(** * Import modules and define nottions *)
 Set Warnings "-notation-overridden".
+Set Warnings "-stdlib-vector".
 From Stdlib Require Import Bool.
 From Stdlib Require Import Init.Nat.
 From Stdlib Require Import Arith.
@@ -7,8 +9,6 @@ From Stdlib Require Import Lia.
 From Stdlib Require Import List. Import ListNotations.
 From Stdlib Require Import Strings.String.
 From Stdlib Require Import Program.Equality.
-
-#[local] Set Warnings "-stdlib-vector".
 From Stdlib Require Fin.
 From Stdlib Require Vector.
 From Stdlib Require Logic.ProofIrrelevance.
@@ -22,13 +22,17 @@ Local Notation "[ x ; .. ; y ]" :=
 Local Notation "v [@ p ]" := (Vector.nth v p) (at level 1).
 Local Notation "<\ H />" := (Fin.of_nat_lt H) (at level 1).
 
+Axiom functional_extensionality : 
+  forall {X Y: Type} (f g: X -> Y), (forall args, f args = g args) -> f = g.
+
+(** * Define Constant Step Functions (CSF) *)
 Inductive csf : nat -> Type :=
   | Zero : csf 0
   | Succ : csf 1
   | Proj (n i: nat) (H: i < n) : csf n
-  | Comp (m n: nat) (f: csf m) (gs: Vector.t (csf n) m) : csf n
-.
+  | Comp (m n: nat) (f: csf m) (gs: Vector.t (csf n) m) : csf n.
 
+(** * Define evaluation of CSF *)
 Definition lt01 : 0 < 1.
 Proof. apply le_n. Qed.
 
@@ -45,24 +49,16 @@ Fixpoint csf_eval {argn: nat} (f: csf argn) : Vector.t nat argn -> nat :=
   | Comp m n f gs =>
       fun (args: Vector.t nat n) =>
         (csf_eval f) (Vector.map (fun f => f args) (Vector.map (csf_eval) gs))
-    end
-.
+  end.
 
+(** * Define non-loop Imp *)
 Inductive ident : Type :=
-  | Id (n: nat)
-.
+  | Id (n: nat).
 
 Inductive aexp : Type :=
   | ANum (n : nat)
   | AId (x : ident)
-  | ASucc (a1: aexp)
-.
-
-Inductive bexp : Type :=
-  | BTrue
-  | BFalse
-  | BEq (a1 a2 : aexp)
-.
+  | ASucc (a1: aexp).
 
 Coercion AId : ident >-> aexp.
 Coercion ANum : nat >-> aexp.
@@ -72,16 +68,9 @@ Declare Scope com_scope.
 
 Notation "<{ e }>" := e
   (e custom com, format "'[hv' <{ '/  ' '[v' e ']' '/' }> ']'") : com_scope.
-
 Notation "x" := x%nat (in custom com at level 0, x constr at level 0).
-
 Notation "x ++"   := (ASucc x) (in custom com at level 50, right associativity).
 Notation "| x |"   := (AId (Id x)) (in custom com at level 85).
-Notation "'true'"  := true (at level 1).
-Notation "'true'"  := BTrue (in custom com at level 0).
-Notation "'false'" := false (at level 1).
-Notation "'false'" := BFalse (in custom com at level 0).
-Notation "x = y"   := (BEq x y) (in custom com at level 70, no associativity).
 
 Open Scope com_scope.
 
@@ -101,25 +90,17 @@ Notation "x '!->' v ';' st" := (st_update x v st)
 
 Notation "x '!->' v" := (st_update x v empty_st) (at level 0, x constr, v at level 200).
 
+(** * Define evaluation of non-loop Imp *)
 Fixpoint aeval (st : state) (a : aexp) : nat :=
   match a with
   | ANum n => n
   | AId x => st x 
   | <{a1 ++}> => S (aeval st a1)
   end.
-
-Definition beval (st : state) (b : bexp) : bool :=
-  match b with
-  | <{true}>      => true
-  | <{false}>     => false
-  | <{a1 = a2}>   => (aeval st a1) =? (aeval st a2)
-  end.
-
 Inductive com : Type :=
   | CSkip
   | CAsgn (x : ident) (a : aexp)
-  | CSeq (c1 c2 : com)
-  | CIf (b : bexp) (c1 c2 : com).
+  | CSeq (c1 c2 : com).
 
 Notation "'skip'"  := CSkip
   (in custom com at level 0) : com_scope.
@@ -130,28 +111,13 @@ Notation "x ; y" := (CSeq x y)
   (in custom com at level 90,
     right associativity,
     format "'[v' x ; '/' y ']'") : com_scope.
-Notation "'if' x 'then' y 'else' z 'end'" := (CIf x y z)
-  (in custom com at level 89, x at level 99, y at level 99, z at level 99,
-    format "'[v' 'if'  x  'then' '/  ' y '/' 'else' '/  ' z '/' 'end' ']'") : com_scope.
-
-Fixpoint app_ntimes {X: Type} (f: X -> X) (n: nat) (x: X) : X :=
-  match n with
-  | O => x
-  | S n' => f (app_ntimes f n' x)
-  end
-.
 
 Fixpoint ceval_st (c: com) (st: state) : state :=
   match c with
   | <{ skip }> => st
   | <{ x := a }> => ((Id x) !-> (aeval st a) ; st)
   | <{ c1; c2 }> => (ceval_st c2) (ceval_st c1 st)
-  | <{ if b then c1 else c2 end }> =>
-        if (beval st b)
-          then ceval_st c1 st
-          else ceval_st c2 st
-  end
-.
+  end.
 
 Fixpoint vec_to_state {n: nat} (m: nat) (v: Vector.t nat n) : state :=
   match n, v with
@@ -160,38 +126,32 @@ Fixpoint vec_to_state {n: nat} (m: nat) (v: Vector.t nat n) : state :=
       let x := Vector.hd v in
       let xs := Vector.tl v in
         ((Id (S(m - n))) !-> x; (vec_to_state m xs))
-  end
-.
+  end.
 
 Definition ceval (c: com) {argn: nat} (args: Vector.t nat argn) : nat :=
   ((ceval_st c) (vec_to_state argn args)) (Id 0).
-
-Axiom functional_extensionality : 
-  forall {X Y: Type} (f g: X -> Y), (forall args, f args = g args) -> f = g.
 
 Fixpoint shift_com i k n : com :=
   match k with
   | O => <{ skip }>
   | S k' => <{ (i + k' + n) := |(i + k')| ;
-               (shift_com i k' n) }>
-  end
-.
+              (shift_com i k' n) }>
+  end.
 
+(** * Define conversion from CSF to Imp *)
 Fixpoint unshift_com i k n : com :=
   match k with
   | O => <{ skip }>
   | S k' => <{ i := |(i + n)| ;
-               (unshift_com (S i) k' n) }>
-  end
-.
+              (unshift_com (S i) k' n) }>
+  end.
 
 Fixpoint comvec_to_com (j: nat) {k: nat} (g_coms: Vector.t com k) : com :=
   match g_coms with
   | [] => <{ skip }>
   | (g_com :: g_coms') =>
         <{ g_com; j := |0|; (comvec_to_com (S j) g_coms') }>
-  end
-.
+  end.
 
 Fixpoint used_state_idx {n: nat} (f: csf n) : nat :=
   match f with
@@ -212,18 +172,14 @@ Fixpoint csf_to_com {argn: nat} (f: csf argn) :=
       let u := max (Vector.fold_left max n (Vector.map used_state_idx gs)) (used_state_idx f) in
       let gs' := Vector.map csf_to_com gs in
         <{ (comvec_to_com (S u) gs');
-           (shift_com 1 n (u+m));
-           (unshift_com 1 m u);
-           (csf_to_com f);
-           (unshift_com 1 n (u+m)) }>
-  end
-.
+          (shift_com 1 n (u+m));
+          (unshift_com 1 m u);
+          (csf_to_com f);
+          (unshift_com 1 n (u+m)) }>
+  end.
 
-Theorem lt_03: 0 < 3.
-Proof. lia. Qed.
-
-Compute (csf_to_com (Comp 3 1 (Proj 3 0 (lt_03)) (Succ :: Succ :: Succ :: []))).  
-
+(** * Proof of csf_eq_com  *)
+(* usefull tactical *)
 Ltac neq_rewrite n m :=
   assert (__Neq__: n <> m) by lia;
   rewrite <- Nat.eqb_neq in __Neq__;
@@ -231,6 +187,7 @@ Ltac neq_rewrite n m :=
   try reflexivity;
   clear __Neq__.
 
+(* Projection *)
 Lemma vec_to_state_shift : forall n m i (v: Vector.t nat n),
   n <= m -> (vec_to_state m v) (Id i) = (vec_to_state (S m) v) (Id (S i)).
 Proof.
@@ -261,6 +218,8 @@ Proof.
       reflexivity.
 Qed.
 
+(* Composition *)
+(* Strong induction principle *)
 Definition Vector_Forall_csf (P : forall n (f: csf n), Prop) {n m} (v : Vector.t (csf n) m) :=
   Vector.fold_right (fun g acc => P n g /\ acc) v True.
 
@@ -271,9 +230,9 @@ Fixpoint csf_ind_strong
   (H_Succ : P 1 Succ)
   (H_Proj : forall n i (H : i < n), P n (Proj n i H))
   (H_Comp : forall m n (f : csf m) (gs : Vector.t (csf n) m),
-     P m f ->
-     Vector_Forall_csf P gs ->
-     P n (Comp m n f gs))
+    P m f ->
+    Vector_Forall_csf P gs ->
+    P n (Comp m n f gs))
   {struct f} : P n f :=
   match f with
   | Zero => H_Zero
@@ -290,6 +249,7 @@ Fixpoint csf_ind_strong
       H_Comp m n f' gs IH_f' (vec_ind gs)
   end.
 
+(* Lemmas on shift/unshift *)
 Lemma shift_is_valid_out_of_range: forall st j i k n,
   j < i + n \/ (i + k + n) <= j ->
   ceval_st (shift_com i k n) st (Id j) = st (Id j).
@@ -346,6 +306,7 @@ Proof.
       simpl. neq_rewrite (j + n) i.
 Qed.
 
+(* Lemmas on Vector.fold_left max i v *)
 Lemma vec_fold_max_lemma_1: forall n (v: Vector.t nat n) i u,
   Vector.fold_left max i v <= u -> i <= u.
 Proof.
@@ -372,6 +333,16 @@ Proof.
   lia.
 Qed.
 
+Lemma vec_fold_max_lemma_4: forall n m (v: Vector.t nat m),
+  n <= Vector.fold_left max n v.
+Proof.
+  induction v.
+  - simpl. lia.
+  - simpl. specialize (vec_fold_max_lemma_2 _ v n (max n h)) as H.
+    lia.
+Qed.
+
+(* used_state_idx is valid *)
 Definition used_state_idx_prop n (f: csf n) :=
   forall i st,
     (used_state_idx f) < i ->
@@ -417,6 +388,7 @@ Proof.
     apply IHgs.
 Qed.
 
+(* Command (csf_to_com f) does not change arguments in state *)
 Definition csf_com_does_not_change_args_prop n (f: csf n) :=
   forall i st,
     0 < i <= n ->
@@ -447,15 +419,6 @@ Proof.
   induction f; simpl; lia.
 Qed.
 
-Lemma vec_fold_max_lemma_4: forall n m (v: Vector.t nat m),
-  n <= Vector.fold_left max n v.
-Proof.
-  induction v.
-  - simpl. lia.
-  - simpl. specialize (vec_fold_max_lemma_2 _ v n (max n h)) as H.
-    lia.
-Qed.
-
 Lemma csf_com_does_not_change_args: forall n f,
   csf_com_does_not_change_args_prop n f.
 Proof.
@@ -481,14 +444,9 @@ Proof.
       lia.
 Qed.
 
-Definition csf_com_eq_with_same_arg_prop n (f: csf n) :=
-  forall st st', 
-    (forall i, 1 <= i <= n -> st (Id i) = st' (Id i)) ->
-    ceval_st (csf_to_com f) st (Id 0) = ceval_st (csf_to_com f) st' (Id 0).
-
+(* (comvec_to_com (S u) v) does not change states[i] when max used_state_idx < i <= u *)
 Lemma comvec_does_not_change_state:
   forall n m (gs: Vector.t (csf n) m) i u st,
-    Vector_Forall_csf csf_com_eq_with_same_arg_prop gs ->
     Vector.fold_left max n (Vector.map used_state_idx gs) < i ->
     i <= u ->
     ceval_st (comvec_to_com (S u) (Vector.map csf_to_com gs)) st (Id i) = st (Id i).
@@ -499,21 +457,26 @@ Proof.
     intros. rewrite IHgs.
     + simpl. neq_rewrite i (S u). apply used_state_idx_is_valid.
       destruct i.
-      * inversion H0.
-      * apply le_n_S. apply le_S_n in H0.
-        apply vec_fold_max_lemma_1 in H0. lia.
-    + apply H.
+      * inversion H.
+      * apply le_n_S. apply le_S_n in H.
+        apply vec_fold_max_lemma_1 in H. lia.
     + destruct i.
-      * inversion H0.
-      * apply le_n_S. apply le_S_n in H0.
+      * inversion H.
+      * apply le_n_S. apply le_S_n in H.
         apply vec_fold_max_lemma_3 with (j:=(max n (used_state_idx h))); lia.
     + lia.
 Qed.
 
+(* The result of (csf_to_com f) does not change when the arguments are the same *)
+Definition csf_com_eq_with_same_arg_prop n (f: csf n) :=
+  forall st st', 
+    (forall i, 1 <= i <= n -> st (Id i) = st' (Id i)) ->
+    ceval_st (csf_to_com f) st (Id 0) = ceval_st (csf_to_com f) st' (Id 0).
+
 Lemma csf_com_eq_with_same_arg_vec:
   forall n m (gs: Vector.t (csf n) m) i u st st',
     Vector_Forall_csf csf_com_eq_with_same_arg_prop gs ->
-    (forall i, 1 <= i /\ i <= n -> st (Id i) = st' (Id i)) ->
+    (forall i, 1 <= i <= n -> st (Id i) = st' (Id i)) ->
     1 <= i <= m ->
     Vector.fold_left max n (Vector.map used_state_idx gs) <= u ->
     ceval_st (comvec_to_com (S u) (Vector.map csf_to_com gs)) st (Id (i + u)) =
@@ -526,10 +489,8 @@ Proof.
     + repeat (rewrite comvec_does_not_change_state; try lia).
       * simpl. repeat (rewrite Nat.eqb_refl). apply H.
         apply H0.
-      * apply H.
       * apply le_n_S. simpl in H2.
         apply vec_fold_max_lemma_3 with (j:=max n (used_state_idx h)); lia.
-      * apply H.
       * apply le_n_S. simpl in H2.
         apply vec_fold_max_lemma_3 with (j:=max n (used_state_idx h)); lia.
     + replace (S i' + u) with (i' + S u) by lia. apply IHgs.
@@ -561,20 +522,6 @@ Proof.
     apply csf_com_eq_with_same_arg_vec; try lia; try assumption.
 Qed.
 
-Definition csf_eq_com_prop (n: nat) (f: csf n) :=
-  csf_eval f = ceval (csf_to_com f).
-
-Lemma vector_forall_lemma: forall n m (gs: Vector.t (csf n) m) P,
-  (forall n f, P n f) ->
-  Vector_Forall_csf P gs.
-Proof.
-  intros. induction gs; intros; unfold Vector_Forall_csf.
-  - simpl. apply I.
-  - simpl. split.
-    + apply H.
-    + apply IHgs.
-Qed.
-
 Lemma csfvec_com_eq_with_same_arg: forall n m (gs: Vector.t (csf n) m) i u st st',
   (forall j, 1 <= j <= n -> st (Id j) = st' (Id j)) ->
   Vector.fold_left max n (Vector.map used_state_idx gs) <= u ->
@@ -590,10 +537,8 @@ Proof.
     + repeat (rewrite comvec_does_not_change_state; try lia).
       * simpl. rewrite Nat.eqb_refl.
         apply csf_com_eq_with_same_arg. apply H.
-      * apply vector_forall_lemma. apply csf_com_eq_with_same_arg.
       * simpl in Hu. apply le_n_S.
         apply vec_fold_max_lemma_3 with (j:=(max n (used_state_idx h))); lia.
-      * apply vector_forall_lemma. apply csf_com_eq_with_same_arg.
       * simpl in Hu. apply le_n_S.
         apply vec_fold_max_lemma_3 with (j:=(max n (used_state_idx h))); lia.
     + apply IHgs; try lia.
@@ -605,6 +550,10 @@ Proof.
       * simpl in Hu.
         apply vec_fold_max_lemma_3 with (j:=(max n (used_state_idx h))); lia.
 Qed.
+
+(* Main theorem *)
+Definition csf_eq_com_prop (n: nat) (f: csf n) :=
+  csf_eval f = ceval (csf_to_com f).
 
 Lemma comvec_to_com_is_valid: forall m n (gs: Vector.t (csf n) m) args i u ,
   1 <= i <= m ->
@@ -627,7 +576,6 @@ Proof.
         simpl in H1. destruct H1 as [H1 _].
         apply (f_equal (fun f => f args)) in H1.
         rewrite H1. reflexivity.
-      * apply vector_forall_lemma. apply csf_com_eq_with_same_arg.
       * simpl in H0. apply le_n_S.
         apply vec_fold_max_lemma_3 with (j:=(max n (used_state_idx h))); lia.
       * lia.
@@ -671,11 +619,8 @@ Proof.
 Qed. 
 
 
-Theorem com_eq_csf : forall (c: com),
+(** Proof com_eq_csf  *)
+  Theorem com_eq_csf : forall (c: com),
   exists (n: nat) (f: csf n), csf_eval f = ceval c.
 Proof.
 Admitted.
-  
-(* i<n で詰まったら proof irrelevance 使ったほうが良さそう *)
-
-(* primrec消してやってみよう *)
